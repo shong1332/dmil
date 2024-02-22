716f95e34468e6bf3b96a07749032999c0554e07
김순홍 <kimsoonhong@gimsunhong-ui-MacBookPro.local>
2024-02-22 16:22:13 +0900

    빌드 오류 해결

--- src/routers/basicRoutes.tsx
+++ src/routers/basicRoutes.tsx
@@ -1,10 +1,5 @@
-
-import  NotFound  from '../pages/nonFound/NotFound';
-import Landing from '../pages/landing/Landing'
-
-
-// 라우터 이슈로 인한 임시 함수 1104 이동훈
-
+import NotFound from '../pages/nonFound/NotFound';
+import Landing from '../pages/landing/Landing';
 
 export const basicRoutes = [
   // Route 에서 사용하는 props 요소들과 동일
