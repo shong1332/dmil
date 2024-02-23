73a58ba59b9edf9d2efed2e0423adf8a81505133
PYE <pyyyyye@PYEui-MacBookPro.local>
2024-02-23 16:40:07 +0900

    test route
    Signed-off-by: PYE <pyyyyye@PYEui-MacBookPro.local>

--- src/routers/basicRoutes.tsx
+++ src/routers/basicRoutes.tsx
@@ -1,8 +1,10 @@
 import NotFound from '../pages/nonFound/NotFound';
 import Landing from '../pages/landing/Landing';
+import Introduce from '../pages/introduce/introduce';
 
 export const basicRoutes = [
   // Route 에서 사용하는 props 요소들과 동일
   { path: '/', element: <Landing /> },
   { path: '*', element: <NotFound /> },
+  { path: '/introduce', element: <Introduce /> },
 ];
